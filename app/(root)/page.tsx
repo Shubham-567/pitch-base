45de054ac993cbf8338a51de75635f149ada181c
Shubham <shubhampatil1356@gmail.com>
2025-05-26 23:51:22 +0530

    Created Navbar with auth, modified theme and layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='animate-fade-in-down text-3xl text-amber-400 font-bold p-30 text-center'>
+        This is a home page
+      </h1>
+    </>
+  );
+}
